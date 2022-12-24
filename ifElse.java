492a4ffa1120bc0a4b0809dda8b39f7477ef13f5
Di1ip <aroraqueen410@gmail.com>
2022-12-24 10:30:14 +0530

    Update ifElse.java

--- ifElse.java
+++ ifElse.java
@@ -4,17 +4,16 @@ public class ifElse{
         System.out.println("hello world");
         Scanner sc=new Scanner(System.in);
         int a;
-        int b;
         System.out.println("enter the value of a");
         a=sc.nextInt();
         
         if(a>0 || a< 0){
             System.out.println("enter the value of b");
-            b=sc.nextInt();
+            int b=sc.nextInt();
             int d = a<0? a*b:a/b;
-            System.out.print 0.ln(d);
+            System.out.println(d);
         }
-        else{
+        else{ 
             System.out.println("bye bye");
         }
     }
