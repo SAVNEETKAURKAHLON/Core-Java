72092d7c7b50953048292973dfe66bc3b38e6f80
Di1ip <aroraqueen410@gmail.com>
2022-12-26 11:02:49 +0530

    Create StarPattern2.java

---
+++ StarPattern2.java
@@ -0,0 +1,12 @@
+public class StarPractice2{
+    public static void main(String args[]){
+        for(int a=0;a<=9;a++){
+            if(a<=5){
+                 for(int b=0;b<a;b++){
+                    System.out.print("*");
+            }
+            }
+           
+        }
+    }
+}
