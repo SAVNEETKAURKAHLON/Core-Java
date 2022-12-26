656fbf8dea0f35558b4232a120874fbc4ae465b1
Di1ip <aroraqueen410@gmail.com>
2022-12-26 09:43:01 +0530

    Create practice1.java

---
+++ practice1.java
@@ -0,0 +1,13 @@
+public class practice1{
+    public static void main(String args[]){
+        System.out.println("hello");
+        int a=5;
+        if(a<5){
+            System.out.println("a is less than 5");
+            a+=5;
+            System.out.println("A is "+a);
+        }
+        else
+            System.out.println("A is not less than 5"); 
+    }
+}
