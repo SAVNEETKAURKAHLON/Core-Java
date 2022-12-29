d39996dd8c4f37ecb66a54520a110d132af0e3bf
SAVNEETKAURKAHLON <119879444+SAVNEETKAURKAHLON@users.noreply.github.com>
2022-12-29 20:27:07 +0530

    Create Starpattern4.java

---
+++ Starpattern4.java
@@ -0,0 +1,10 @@
+public class Starpattern4{
+    public static void main(String args[]){
+        for(int a=0;a<=5;a++){
+            for(int k=0;k<a;k++){
+                System.out.print("*");
+            }
+                System.out.println(" ");
+        }
+    }
+}
