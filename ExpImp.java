f9644c703b700ce2084a7168e7484c2cc8de57ec
SAVNEETKAURKAHLON <119879444+SAVNEETKAURKAHLON@users.noreply.github.com>
2022-12-29 21:27:53 +0530

    Create ExpImp.java

---
+++ ExpImp.java
@@ -0,0 +1,11 @@
+public class ExpImp{
+    public static void main(String args[]){
+        int a=10;
+        int b=2;
+        float c=7;
+        int d=a+b;
+        System.out.println("sum is "+d);
+        int e=(int)(c+b);
+        System.out.println("sum is "+e);
+    }
+}
