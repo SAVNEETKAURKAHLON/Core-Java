e9d176f8e5a68dfba8b12e698cc4861eb988d884
Di1ip <aroraqueen410@gmail.com>
2022-12-30 10:51:15 +0530

    Create Kdiamond.java

---
+++ Kdiamond.java
@@ -0,0 +1,34 @@
+public class Kdiamond{
+    public static void main(String args[]){
+        for(int a=0;a<5;a++){
+            for(int b=5;b>a;b--){
+                 System.out.print("*");
+            }
+            for(int c=0;c<a;c++){
+                System.out.print(" ");
+            }
+               for(int j=0;j<a;j++){
+                System.out.print(" ");
+            }
+            for(int k=a;k<5;k++){
+                System.out.print("*");
+            }
+                 System.out.println("");
+        }
+        for(int a=1;a<5;a++){
+             for(int k=0;k<=a;k++){
+                System.out.print("*");
+            }
+            for(int j=4;j>a;j--){
+                System.out.print(" ");
+            }
+             for(int j=4;j>a;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<=a;k++){
+                System.out.print("*");
+            }
+                System.out.println("");
+        }
+    }
+} 
