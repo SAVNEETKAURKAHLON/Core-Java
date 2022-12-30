c76fdd8bd7b1d9340f22be9bf6d5bd1450457d0a
SAVNEETKAURKAHLON <119879444+SAVNEETKAURKAHLON@users.noreply.github.com>
2022-12-30 21:52:33 +0530

    Update ExpImp.java

--- ExpImp.java
+++ ExpImp.java
@@ -1,11 +1,13 @@
 public class ExpImp{
     public static void main(String args[]){
         int a=10;
-        int b=2;
+        double b=2;
         float c=7;
-        int d=a+b;
+        int d=(int)(a+b+c);
         System.out.println("sum is "+d);
-        int e=(int)(c+b);
+        float e=(float)(a+c+b);
         System.out.println("sum is "+e);
+        double f=(double)(a+b+c);
+        System.out.println("sum is "+f);
     }
 }
