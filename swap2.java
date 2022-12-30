7c123a82cd886f02197f3054823fede5b19e6537
Di1ip <aroraqueen410@gmail.com>
2022-12-30 09:21:53 +0530

    Update swap2.java

--- swap2.java
+++ swap2.java
@@ -1,9 +1,15 @@
-public class swap2{
+import java.util.Scanner;
+public class Swap2{
     public static void main(String args[]){
         System.out.println("helo");
+        Scanner sc=new Scanner(System.in);
         int a=2;
         int b=4;
-        System.out.println("a is "+b);
-        System.out.println("b is "+a);
+         a=a+b;
+         b=a-b;
+         a=a-b;
+         System.out.println("the value of a is "+a);
+        System.out.println("the value of b is "+b);
+
     }
 }
