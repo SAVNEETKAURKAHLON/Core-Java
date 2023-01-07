92feb28700ee6cac6429e5a6ae6d3ee538204361
Di1ip <aroraqueen410@gmail.com>
2023-01-07 09:14:37 +0530

    Update SearchingArray.java

--- SearchingArray.java
+++ SearchingArray.java
@@ -7,14 +7,17 @@ public class SearchingArray{
         int size=sc.nextInt();
         a=new int[size];
         for(int i=0;i<size;i++){
-            System.out.println("enter the value at "+a[i]);
+            System.out.println("enter the value at "+i);
             a[i]=sc.nextInt();
         }
-    //    for(int i=0;i<size;i++){
-    //     System.out.println("enter");
-    //     int b=a[i];
-    //        b=sc.nextInt();
-        // System.out.println(+a[i]);
-    //    }
+        System.out.println("enter the element to search");
+        int b=sc.nextInt();
+       for(int i=0;i<size;i++){
+        if(b==a[i]){
+
+        System.out.println("element is found at index "+i);
+            break;
+        }
+       }
     }
 }
