240d34a2201d0686238c94dbfe3396b39f353151
SAVNEETKAURKAHLON <119879444+SAVNEETKAURKAHLON@users.noreply.github.com>
2023-01-12 07:37:57 +0530

    11jan

---
+++ inheritance/Member.java
@@ -0,0 +1,13 @@
+package inheritance;
+public class Member{
+    
+       public String name;
+       public int age;
+       public int number;
+       public String address;
+       public int salary;
+
+        public void printSalary(){
+        System.out.println("salary is "+salary);
+    }
+}
