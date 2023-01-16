a3251afa7382d85b7d0db0f34d208f1c6d42ce20
Di1ip <aroraqueen410@gmail.com>
2023-01-16 09:42:21 +0530

    16jan

--- vehicle/PoweredVehicle.java
+++ vehicle/PoweredVehicle.java
@@ -1,12 +1,9 @@
 package vehicle;
 public class PoweredVehicle extends Vehicle{
-    PoweredVehicle(){
-        String myFuelType="petrol";
-    }
+    String myFuelType="petrol";
     public static void main(String args[]){
         PoweredVehicle poweredVehicle=new PoweredVehicle();
-        poweredVehicle.vehicle();
-        System.out.println(myBrand+" "+mymodel+" "+myFuelType);
+        System.out.println(poweredVehicle.myBrand+" "+poweredVehicle.myModel+" "+poweredVehicle.myFuelType);
 
     }
 }
--- vehicle/Vehicle.java
+++ vehicle/Vehicle.java
@@ -1,8 +1,6 @@
 package vehicle;
 public class Vehicle{
-    vehicle(){
         String myBrand="maruti";
         String myModel="swift";
-    }
     
 }
