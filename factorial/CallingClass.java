0843694665478be924d13560806d7f7aabbfcb11
Di1ip <aroraqueen410@gmail.com>
2023-01-16 10:47:34 +0530

    factorial

---
+++ factorial/CallingClass.java
@@ -0,0 +1,8 @@
+package factorial;
+
+public class CallingClass {
+    public static void main(String args[]){
+        Factorial factorial=new Factorial(4);
+        factorial.starPattern();
+    }
+}
