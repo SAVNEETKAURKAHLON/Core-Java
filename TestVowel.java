23f375313bb7e28d926a1b36ddc1ae671955cec1
SAVNEETKAURKAHLON <119879444+SAVNEETKAURKAHLON@users.noreply.github.com>
2023-01-18 21:39:01 +0530

    18jan

---
+++ TestVowel.java
@@ -0,0 +1,14 @@
+public class TestVowel{
+      public static void main(String args[]){
+        Scanner sc=new Scanner(System.in)    ;
+        System.out.println("enter the word");
+        String a[]=sc.next();
+        String temp="";
+        String b[10]={"a""e""i""o""u""A""E""I""O""U"};
+        for(int i=a.length()-1;i>=0;i--){
+            if(int a[1]==b[]){
+                System.out.print("hello");
+            }
+        }
+      }
+}
