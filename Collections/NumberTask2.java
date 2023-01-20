080f470c28b83674e88d24dda0ffb899ccf9566b
SAVNEETKAURKAHLON <119879444+SAVNEETKAURKAHLON@users.noreply.github.com>
2023-01-20 20:11:19 +0530

    20jan
    number task update
    cards
    cards output

--- Collections/NumberTask2.java
+++ Collections/NumberTask2.java
@@ -21,7 +21,23 @@ public class NumberTask2{
             }
           
         }
-        System.out.println("order is");
+         
+        System.out.println("descending order is");
+
+        for(int k=0;k<size;k++){
+            System.out.println(numbertask.get(k));
+        }
+         for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(numbertask.get(i)<numbertask.get(j)){
+                       int temp=numbertask.get(i);
+                       numbertask.set(i, numbertask.get(j));
+                       numbertask.set(j,temp);
+                }
+            }
+          
+        }
+        System.out.println("ascending order is");
 
         for(int k=0;k<size;k++){
             System.out.println(numbertask.get(k));
--- cards.java
+++ cards.java
@@ -1,7 +1,18 @@
 public class cards {
-    String suit[]={"s","d","c","h"};
+    private String suit[]={"s","d","c","h"};
     int value[]={1,2,3,4,5,6,7,8,9,10,11,12,13};
-    int getS(){
-        
+    int ran;
+    int val;
+    int getSuit(int a){
+        return Suit[];
+    }
+    int setSuit(int a){
+        this.ran=a;
+    }
+    int getValue(int a){
+        return value[];
+    }
+    int setValue(int b){
+        this.val=b;
     }
 }
